a911eb773595c9ef17bd088bc072f91c3ddb31cc
Constantl57 <constant.lafosse@gmail.com>
2026-09-18 23:36:41 +0200

    Update

---
+++ src/components/TweetPreview.ts
@@ -0,0 +1,5 @@
+export 
+
+
+créez un composant qui reçoit un tweet en argument et affiche : • le nom de l'auteur. • son nom d'utilisateur, précédé du caractère @. • la date dans un format lisible. • le contenu du tweet.
+
---
+++ src/data/tweets.ts
@@ -0,0 +1,26 @@
+import type {Tweet} from '../types/Tweet';
+
+export const tweets: Tweet[] = [
+  {
+    id: "1",
+    authorName: "Victoria University of Manchester.",
+    authorHandle: "@Victoria University of Manchester.",
+    content: "I propose to consider the question, ‘Can machines think?’ This should begin with definitions of the meaning of the terms ‘machine’ and ‘think’. The definitions might be framed so as to reflect so far as possible the normal use of the words, but this attitude is dangerous. If the meaning of the words ‘machine’ and ‘think’ are to be found by examining how they are commonly used it is difficult to escape the conclusion that the meaning and the answer to the question, ‘Can machines think?’ is to be sought in a statistical survey such as a Gallup poll. But this is absurd. Instead of attempting such a definition I shall replace the question by another, which is closely related to it and is expressed in relatively unambiguous words.",
+    image: {
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/500px-Ada_Lovelace_portrait.jpg",
+      alt: "portrait d'Ada Lovelace "
+    },
+    createdAt: "2026-01-01T00:00:00Z"
+  },
+    {
+      id: "2",
+      authorName: "zzzzzz",
+      authorHandle: "@rrrrrrrr",
+      content: "qdzdzqgsfsdg",
+      image: {
+        url: "https://upload.wikimedia.org/wikipedia/commons/5/55/Grace_Hopper.jpg",
+        alt: "Grace Hopper"
+      },
+      createdAt: "2026-01-01T00:00:00Z"
+    }
+];
